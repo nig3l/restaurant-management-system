9d2e3e3fcb2d1b002352713386bff3870743cdfb
Nigel <nigelchimwene.gmail.com>
2023-03-23 11:45:57 +0300

    second commit

---
+++ loginpage.py
@@ -0,0 +1,40 @@
+
+import tkinter as tk
+from tkinter import messagebox
+
+# Creating the main window
+root = tk.Tk()
+root.title("BoozeCAFE lOGIN FORM")
+root.geometry("600x600")
+
+# Function to authenticate the user
+def authenticate_user():
+    # Getting the username and password entered by the user
+    username = username_entry.get()
+    password = password_entry.get()
+    
+    # Check if the username and password are correct
+    if username == "admin" and password == "admin123":
+        messagebox.showinfo("Login Successful", "Welcome, " + username + "!")
+    else:
+        messagebox.showerror("Login Error", "Invalid username or password")
+
+# Creating the labels and text entry fields
+username_label = tk.Label(root, text="Username:")
+username_label.grid(row=0, column=0)
+
+username_entry = tk.Entry(root)
+username_entry.grid(row=0, column=1)
+
+password_label = tk.Label(root, text="Password:")
+password_label.grid(row=1, column=0)
+
+password_entry = tk.Entry(root, show="*")
+password_entry.grid(row=1, column=1)
+
+# Creating the login button
+login_button = tk.Button(root, text="Login", command=authenticate_user)
+login_button.grid(row=2, column=1)
+
+# Running the main loop
+root.mainloop()
